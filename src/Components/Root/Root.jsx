3a69ad48d11917d67bb9bc210615a73a26fa889e
jerin-arpa <ismatjerrin3@gmail.com>
2023-10-09 06:34:24 +0600

    Initial project setup

---
+++ src/Components/Root/Root.jsx
@@ -0,0 +1,17 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "../Navbar/Navbar";
+import Footer from "../Footer/Footer";
+
+const Root = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            <div className="container mx-auto">
+                <Outlet></Outlet>
+            </div>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default Root;
